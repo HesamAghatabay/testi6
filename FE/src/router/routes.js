9e99612ef7b7cf251680edff3c4e5b360df44f6b
Hesam Aghatabay <program.h.p.2023@gmail.com>
2025-05-18 15:09:10 +0330

    update

--- FE/src/router/routes.js
+++ FE/src/router/routes.js
@@ -3,16 +3,27 @@ const routes = [
     path: '/',
     component: () => import('layouts/MyLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/register', component: () => import('pages/RegisterPage.vue') },
+      { path: '/login', component: () => import('pages/LoginPage.vue') },
+      { path: '/sendVerify', component: () => import('pages/SendVerify.vue') },
+      { path: '/confirmLogin', component: () => import('pages/ConfirmPassword.vue') },
+      { path: '/profile', component: () => import('pages/ProfilePage.vue') },
+      { path: '/category', component: () => import('pages/CategoryPage.vue') },
+      { path: '/create-category', component: () => import('pages/CreateCategory.vue') },
+      { path: '/edit-category', component: () => import('pages/EditCategory.vue') },
+      { path: '/post', component: () => import('pages/PostPage.vue') },
+      { path: '/create-post', component: () => import('pages/CreatePost.vue') },
+      { path: '/edit-post', component: () => import('pages/EditPost.vue') },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
 ]
 
 export default routes
